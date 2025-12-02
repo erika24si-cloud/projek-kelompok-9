738c2d8b32e697c7b1a44de5ccca461c84bb82cb
syahrul <syahrul24si@mahasiswa.pcr.ac.id>
2025-12-03 05:48:59 +0700

    Fix merge conflict di routes & kategoriAsetController

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\kategoriAsetController;
+use App\Http\Controllers\KategoriAsetController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\wargaController;
+use App\Http\Controllers\WargaController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\AsetController;
+
+// -------------------
+// HALAMAN AWAL
+// -------------------
 
 Route::get('/', function () {
-    return view('welcome'); });
-    
+    return view('welcome');
+});
+
 Route::get('/ketua', function () {
     return view('ketua');
 });
@@ -19,31 +25,33 @@ Route::get('/anggota', function () {
     return view('anggota');
 });
 
-Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::resource('kategoriAset', kategoriAsetController::class);
-Route::resource('products', \App\Http\Controllers\ProductController::class);
+// -------------------
+// DASHBOARD
+// -------------------
 
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-/////////
-Route::get('/bina', [DashboardController::class, 'index']);
-Route::get('/login', [AuthController::class, 'login']);
-Route::get('/register', [AuthController::class, 'register']);
-// Catch-all: try to load views/pages/{slug}.blade.php
-Route::get('/  ', [PageController::class, 'show'])->where('slug', '.*');
+// -------------------
+// RESOURCE CONTROLLER
+// -------------------
 
-Route::resource('kategoriAset', kategoriAsetController::class);Route::resource('products', \App\Http\Controllers\ProductController::class);
-//--------------------------------------------------------------------------//
+Route::resource('kategoriAset', KategoriAsetController::class);
+Route::resource('products', ProductController::class);
+Route::resource('aset', AsetController::class);
+Route::resource('warga', WargaController::class);
+
+// -------------------
+// AUTH
+// -------------------
+
+Route::get('/login',  [AuthController::class, 'login'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::get('/forgot-password', function () {
     return view('auth.forgot-password');
 })->name('password.request');
 
-Route::get('/sample-page', function () {
-    return view('other.sample');
-})->name('sample.page');
+// -------------------
+// SLUG PAGE CONTROLLER
+// -------------------
 
-Route::resource('kategoriAset', KategoriAsetController::class);
-
-Route::resource('aset', AsetController::class);
-
-Route::resource('warga', WargaController::class);
-
+Route::get('/{slug}', [PageController::class, 'show'])->where('slug', '.*');
