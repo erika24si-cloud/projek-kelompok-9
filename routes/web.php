9d3c34313434f1ea88d52d38e436a00cfdf5a6ed
syahrul <syahrul24si@mahasiswa.pcr.ac.id>
2025-12-03 05:52:11 +0700

    Fix conflict routes

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,6 @@ use App\Http\Controllers\WargaController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\AsetController;
-
-// -------------------
-// HALAMAN AWAL
-// -------------------
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -25,33 +20,17 @@ Route::get('/anggota', function () {
     return view('anggota');
 });
 
-// -------------------
-// DASHBOARD
-// -------------------
-
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-
-// -------------------
-// RESOURCE CONTROLLER
-// -------------------
 
 Route::resource('kategoriAset', KategoriAsetController::class);
 Route::resource('products', ProductController::class);
 Route::resource('aset', AsetController::class);
 Route::resource('warga', WargaController::class);
 
-// -------------------
-// AUTH
-// -------------------
-
 Route::get('/login',  [AuthController::class, 'login'])->name('login');
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::get('/forgot-password', function () {
     return view('auth.forgot-password');
 })->name('password.request');
-
-// -------------------
-// SLUG PAGE CONTROLLER
-// -------------------
 
 Route::get('/{slug}', [PageController::class, 'show'])->where('slug', '.*');
