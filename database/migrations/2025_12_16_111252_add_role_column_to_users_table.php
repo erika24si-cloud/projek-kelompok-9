6c791a8fb690b9364189c42cd1e1160fad29141b
erika <erika24si@mahasiswa.pcr.ac.id>
2025-12-16 23:55:40 +0700

    ubah

--- database/migrations/2025_12_16_111252_add_role_column_to_users_table.php
+++ database/migrations/2025_12_16_111252_add_role_column_to_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             Schema::table('users', function (Blueprint $table) {
-            //$table->string('role')->default('viewer')->after('name'); 
+            $table->string('role')->after('name'); 
         });
         });
     }
