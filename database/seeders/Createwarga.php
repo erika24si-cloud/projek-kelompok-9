2ea9495b12d981be91bf563f79d2ddb7caa5736d
erika <erika24si@mahasiswa.pcr.ac.id>
2026-01-06 23:25:18 +0700

    seeder3

--- database/seeders/Createwarga.php
+++ database/seeders/Createwarga.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Faker\Factory;
+use Faker\Factory as Faker;
 use Illuminate\Support\Facades\DB;
 
 class Createwarga extends Seeder
