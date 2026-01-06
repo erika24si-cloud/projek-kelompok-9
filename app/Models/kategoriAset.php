ae505bc7c20d6cdb84a6b8ee1e451860acf295b2
syahrul <syahrul24si@mahasiswa.pcr.ac.id>
2026-01-07 00:49:46 +0700

    final 1

--- app/Models/kategoriAset.php
+++ app/Models/kategoriAset.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class kategoriAset extends Model
 {
-    protected $table = 'kategori_aset';
+    protected $table = 'kategoriAset';
     protected $primaryKey = 'kategori_id';
     protected $fillable = ['nama', 'kode', 'deskripsi'];
 }
