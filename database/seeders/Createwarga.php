d188ca9a60403e1c284842a8456a70c4c5349d7d
erika <erika24si@mahasiswa.pcr.ac.id>
2026-01-06 23:21:34 +0700

    seeder

---
+++ database/seeders/Createwarga.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory;
+use Illuminate\Support\Facades\DB;
+
+class Createwarga extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = \Faker\Factory::create();
+
+        for ($i = 1; $i <= 7; $i++) {
+            DB::table('warga')->insert([
+                'no_ktp' => $faker->unique()->nik(), 
+                'nama' => $faker->name,
+                'jenis_kelamin' => $faker->randomElement(['Laki-laki', 'Perempuan']),
+                'agama' => $faker->randomElement(['Islam', 'Kristen', 'Katolik', 'Hindu', 'Buddha', 'Khonghucu']),
+                'pekerjaan' => $faker->jobTitle,
+                'telp' => $faker->phoneNumber,
+                'email' => $faker->unique()->safeEmail,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
