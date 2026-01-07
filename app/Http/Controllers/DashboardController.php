c6a03d1225eb054166ef4e612590e13d3ba525bf
syahrul <syahrul24si@mahasiswa.pcr.ac.id>
2026-01-07 07:34:20 +0700

    revisi dikit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Aset;
+use App\Models\aset;
 use App\Models\kategoriAset;
 use App\Models\mutasiAset;
 use App\Models\pemeliharaanAset;
